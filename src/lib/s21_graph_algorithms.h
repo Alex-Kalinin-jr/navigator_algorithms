1f037ef7b7af5f87d9c54389df0c2842676e87bf
alex <alex@alex.alex>
2023-09-14 09:54:24 +0700

    2nd test was passed at night. to be found - where is wrong change

--- src/lib/s21_graph_algorithms.h
+++ src/lib/s21_graph_algorithms.h
@@ -14,7 +14,7 @@ namespace s21 {
 using std::vector;
 
 const int kNumAnts = 100;
-const int kNumIterations = 15000;
+const int kNumIterations = 2000;
 const double kAlpha = 1.0;
 const double kBeta = 2.0;
 const double kRHO = 0.5;
